b0e9d736e244d45bb7b98a10f71ca89af7618da0
pbkou <2060408k@student.gla.ac.uk>
2017-02-20 20:59:16 +0000

    Action Bar   -"Admin Page"  Added new drawable for "admin page"

---
+++ app/src/main/java/com/example/pbkou/smarthouse/House_Settings.java
@@ -0,0 +1,45 @@
+package com.example.pbkou.smarthouse;
+
+import android.bluetooth.le.ScanSettings;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class House_Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_house__settings);
+
+        //Set toolbar
+        //set the toolbar
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch(id) {
+            case R.id.house_settings:
+                Intent intent = new Intent(this,House_Settings.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/example/pbkou/smarthouse/MainActivity.java
+++ app/src/main/java/com/example/pbkou/smarthouse/MainActivity.java
@@ -8,13 +8,16 @@ import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanResult;
 import android.bluetooth.le.ScanSettings;
+import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,6 +27,7 @@ import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -60,6 +64,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //set the toolbar
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        //Set content_main padding
+        int actionBarHeight = 0;
+
+        TypedValue tv = new TypedValue();
+        if (getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
+            actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data, getResources().getDisplayMetrics());
+        }
+        RelativeLayout r_layout = (RelativeLayout) findViewById(R.id.content_main);
+        r_layout.setPadding(16,actionBarHeight,16,16);
 
 
         // create the adapter used to populate the list of beacons and attach it to the widget
@@ -134,7 +152,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.action_bar, menu);
         return true;
     }
 
@@ -148,6 +166,10 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.action_mode_lowlat:
                 scanMode = ScanSettings.SCAN_MODE_LOW_LATENCY;
+                break;
+            case R.id.house_settings:
+                Intent intent = new Intent(this,House_Settings.class);
+                startActivity(intent);
                 break;
             default:
                 break;
