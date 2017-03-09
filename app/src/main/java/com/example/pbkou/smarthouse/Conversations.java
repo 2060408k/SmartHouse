d05153c75bb9f6617ff1fc9532921b42fbd34035
pbkou <2060408k@student.gla.ac.uk>
2017-03-09 13:26:50 +0000

    Design changes

--- app/src/main/java/com/example/pbkou/smarthouse/Conversations.java
+++ app/src/main/java/com/example/pbkou/smarthouse/Conversations.java
@@ -178,9 +178,6 @@ public class Conversations extends AppCompatActivity {
             case R.id.house_settings:
                 startActivity(new Intent(this,House_Settings.class));
                 break;
-            case R.id.view_conversations:
-                startActivity(new Intent(this,Conversations.class));
-                break;
             case R.id.activity_notifications:
                 startActivity(new Intent(this,notifications.class));
                 break;
--- app/src/main/java/com/example/pbkou/smarthouse/notifications.java
+++ app/src/main/java/com/example/pbkou/smarthouse/notifications.java
@@ -33,9 +33,6 @@ public class notifications extends AppCompatActivity {
             case R.id.house_settings:
                 startActivity(new Intent(this, House_Settings.class));
                 break;
-            case R.id.view_conversations:
-                startActivity(new Intent(this, Conversations.class));
-                break;
             case R.id.activity_notifications:
                 startActivity(new Intent(this, notifications.class));
                 break;
